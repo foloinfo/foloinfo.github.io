bd9a77a1139f906ce371b23f2932dc9a50acb056
foloinfo <yoshiaki.tsutsumi@byus.me>
2023-02-25 16:53:18 +0900

    ADD date on PostLinks

--- components/posts/Links.js
+++ components/posts/Links.js
@@ -1,21 +1,25 @@
 import Link from "next/link";
+import DateLabel from "./DateLabel";
 
 const PostLinks = ({
   posts,
 })=> {
 
-  console.log(posts);
   return (
     <div>
       {posts.map((post) => (
-        <div key={post.node.id}>
+        <div key={post.node.id} style={{
+          marginBottom: '1rem',
+        }}>
           <Link href={`/posts/${post.node._sys.filename}`}>
             <a style={{
               fontSize: '1.2rem',
               fontWeight: 'bold',
             }}>
-            {post.node.title}</a>
+              {post.node.title}
+            </a>
           </Link>
+          <DateLabel datetime={post.node.date} />
         </div>
       ))}
     </div>
