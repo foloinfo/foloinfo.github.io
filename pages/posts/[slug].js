15f52986638b0cb7fcac7eb91fc24335483b5b3f
foloinfo <yoshiaki.tsutsumi@byus.me>
2023-02-25 15:07:20 +0900

    MOD posts/[slug] style

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,6 +1,7 @@
 import { Layout } from "../../components/Layout";
 import { useTina } from "tinacms/dist/react";
 import { client } from "../../.tina/__generated__/client";
+import { TinaMarkdown } from "tinacms/dist/rich-text";
 
 export default function Home(props) {
   // data passes though in production mode and data is updated to the sidebar data in edit-mode
@@ -10,17 +11,12 @@ export default function Home(props) {
     data: props.data,
   });
 
+  const title = data.post.title
+  const content = data.post.body
   return (
     <Layout>
-      <code>
-        <pre
-          style={{
-            backgroundColor: "lightgray",
-          }}
-        >
-          {JSON.stringify(data.post, null, 2)}
-        </pre>
-      </code>
+      <h1>{title}</h1>
+      <TinaMarkdown content={content} />
     </Layout>
   );
 }
